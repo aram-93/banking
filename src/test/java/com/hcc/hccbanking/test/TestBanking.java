327e5be14545d2fea5f303d6d595687fe9d67363
aram-93 <mkhitaryan.aram.93@gmail.com>
2025-05-09 13:00:35 +0400

    added new test

---
+++ src/test/java/com/hcc/hccbanking/test/TestBanking.java
@@ -0,0 +1,14 @@
+package com.hcc.hccbanking.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestBanking {
+
+    @Test
+    public void testGetCreditPageViewItemList(){
+        Assert.assertTrue(true);{
+
+        }
+    }
+}
