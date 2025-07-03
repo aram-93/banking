bceaaa8041d0db0c209d93689dedde23c831f5ba
aram-93 <mkhitaryan.aram.93@gmail.com>
2025-07-03 19:08:39 +0400

    Updated testng.xml file

--- src/test/java/com/hcc/hccbanking/test/tt/TestClassUsingParameters.java
+++ src/test/java/com/hcc/hccbanking/test/tt/TestClassUsingParameters.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class TestClassUsingParameters {
 
-    @Test(groups = {"ttfb"}, description = "my test description", dataProvider = "dp")
+    @Test(groups = {"ttfb"}, description = "my test description")
     public void testMethod() {
         String name = "jskfhhTest";
         int age = 20;
