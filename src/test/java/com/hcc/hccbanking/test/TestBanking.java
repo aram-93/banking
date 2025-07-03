3c2afa6c9719ef9fa201d02af4edf4ae5f46fc67
aram-93 <mkhitaryan.aram.93@gmail.com>
2025-07-03 18:37:44 +0400

    Updated pom.xml file

--- src/test/java/com/hcc/hccbanking/test/TestBanking.java
+++ src/test/java/com/hcc/hccbanking/test/TestBanking.java
@@ -1,12 +1,16 @@
 package com.hcc.hccbanking.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+@Slf4j
 public class TestBanking {
 
     @Test
     public void testGetCreditPageViewItemList(){
+
+        log.info("all is good");
         Assert.assertTrue(true);{
 
         }
